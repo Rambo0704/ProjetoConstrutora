2f0e835666138feaa87794eaa1e4a1f635b11282
Gabriel Antonio Rambo Xavier <gabriel.xavier@sipam.gov.br>
2025-05-22 16:46:14 -0300

    fix: models

--- backend/cadastro/models.py
+++ backend/cadastro/models.py
@@ -20,5 +20,6 @@ class Produto(models.Model):
     descricao_produto = models.TextField()
     preco_produto = models.DecimalField(max_digits=10,decimal_places=2)
     quantidade_estoque = models.IntegerField()
-    imagem_produto = models.ImageField(upload_to='produtos/',null=True,blank=True)
+    imagem_produto_url = models.URLField(max_length=200, blank=True, null=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
     
